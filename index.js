73a828da0eb2d5e11ca70b7051f60ede1a90f014
denniman <dennimanadams@gmail.com>
2022-11-05 07:45:25 +0100

    add api

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+const mathematicalOperation = {
+  addition: "addition",
+  subtraction: "subtraction",
+  multiplication: "multiplication",
+};
+
+const responseObject = {
+  slackUsername: "denniman02",
+};
+
+app.post("/api", (req, res) => {
+  const { operation_type, y, x } = req.body;
+
+  if (!operation_type) {
+    res.status(400).send("missing operation_type");
+    return;
+  }
+
+  if (!y || !x) {
+    res.status(400).send("missing x and y integer");
+    return;
+  }
+  let result;
+
+  if (typeof y !== "number" || typeof x !== "number") {
+    res.status(400).send("x and y must be an integer value");
+    return;
+  }
+
+  switch (mathematicalOperation[operation_type]) {
+    case "addition":
+      result = y + x;
+      res.send({
+        ...responseObject,
+        result,
+        operation_type: mathematicalOperation[operation_type],
+      });
+      break;
+    case "subtraction":
+      x > y ? (result = x - y) : (result = y - x);
+      res.send({
+        ...responseObject,
+        result,
+        operation_type: mathematicalOperation[operation_type],
+      });
+      break;
+    case "multiplication":
+      result = y * x;
+      res.send({
+        ...responseObject,
+        result,
+        operation_type: mathematicalOperation[operation_type],
+      });
+      break;
+    default:
+      res.status(400).send(`invalid operation_type ${operation_type}`);
+  }
+});
+
+app.listen(port, () => console.log(`app running on port ${port}`));
